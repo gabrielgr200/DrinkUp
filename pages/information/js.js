915ec47728aed3e8b07a772f7581db745c6d5fcf
Gabriel Gomes Rodrigues <138343762+gabrielgr200@users.noreply.github.com>
2023-10-29 23:33:05 -0300

    drinkUp

--- pages/information/js.js
+++ pages/information/js.js
@@ -6,12 +6,10 @@ fetch('http://52.72.243.224:7000/registros/register')
     const passwordInput = document.getElementById('password');
 
     if (data.length > 0) {
-      // Preencha os campos de input com os dados do primeiro registro (ou outro registro, se necessário).
       idInput.value = data[0].id || '';
       nameInput.value = data[0].name || '';
       passwordInput.value = data[0].password || '';
     } else {
-      // Se não houver dados, defina os campos de input como vazios.
       idInput.value = '';
       nameInput.value = '';
       passwordInput.value = '';
